c6e89a1fd8cfbcf91289e17da9dd0204bb3682b7
Bilal Nassaralah <bilalnassaralahpro@gmail.com>
2025-09-19 18:12:14 +0200

    chore: constants and Supabase init cleanup

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 class Constants {
   // Supabase Configuration
+  // TODO: Replace with your real Supabase credentials before shipping.
   static const String supabaseUrl = 'https://zoxiyajiiuryhzssiywo.supabase.co';
   static const String supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpveGl5YWppaXVyeWh6c3NpeXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMjk0MjMsImV4cCI6MjA3MTcwNTQyM30.TCWiWBB_v0MEt2VSFpNXUo-lJFnp5e-d_MRtS3xa2hw';
